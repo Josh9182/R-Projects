f70a4dd285f4174f6761e0bcf59f58bba3a5a1b2
Josh Lewis <jlewis200413@gmail.com>
2024-07-17 14:57:49 -0600

    Update plot_visualization.R

--- Sample Data/plot_visualization.R
+++ Sample Data/plot_visualization.R
@@ -60,35 +60,87 @@ server <- function(input, output, session) {
                 mutate_if(is.character, function(x) str_replace_all(x, ",", ""))}
         else {
             stop("Empty Dataframe unable to visualize, please upload different file.")}})
-
+    
     observe({
         req(clean_dt())
-    
+        
         updateCheckboxGroupInput(session, "x_value", choices = colnames(clean_dt()), selected = colnames(clean_dt())[1])
         updateSelectInput(session, "y_value", choices = colnames(clean_dt()), selected = colnames(clean_dt())[2])
-    
+        
         numeric_data <- clean_dt() %>%
             select(where(is.numeric))
-    
+        
         updateSliderInput(session, "xrange", min = min(numeric_data), max = max(numeric_data), 
-            value = c(min(numeric_data), max(numeric_data)))
+                          value = c(min(numeric_data), max(numeric_data)))
         updateSliderInput(session, "yrange", min = min(numeric_data), max = max(numeric_data), 
-            value = c(min(numeric_data), max(numeric_data)))})
-
+                          value = c(min(numeric_data), max(numeric_data)))})
+    
     output$plot_vis <- renderPlot({
-        req(dt())
+        req(clean_dt(), input$animated == "No")
+        
+        plot_dt <- clean_dt() %>%
+            select(all_of(c(input$x_value, input$y_value)))
+        
+        if (input$plot_type == "bar") {
+            bar_plot <- ggplot(plot_dt, aes_string(x = input$x_value, y = input$y_value, fill = input$x_value)) +
+                geom_bar(stat = "identity", position = "dodge", linewidth = 1, color = "black") +
+                coord_flip() + 
+                labs(title = paste(paste(input$x_value, sep = ", "), "by", input$y_value), x = paste(input$x_value, sep = ","), y = input$y_value) +
+                
+                theme_minimal() +
+                theme(panel.grid = element_line(linewidth = .5, color = "black"),
+                      axis.text.x = element_text(size = 15, face = "bold", color = "black", 
+                                                 margin = margin(b = 10)),
+                      axis.text.y = element_text(size = 15, face = "bold", color = "black", 
+                                                 margin = margin(l = 10)),
+                      plot.title = element_text(size = 25, hjust = -.5),
+                      axis.title = element_text(size = 20),
+                      legend.text = element_text(size = 15),
+                      legend.title = element_text(size = 18))
+            print(bar_plot)}
+        
+        else if (input$plot_type == "scatter") {
+            scatter_plot <- ggplot(plot_dt, aes_string(x = input$x_value, y = input$y_value, fill = input$x_value)) +
+                geom_point(size = 6, alpha = .8) +
+                coord_flip() + 
+                labs(title = paste(paste(input$x_value, sep = ", "), "by", input$y_value), x = paste(input$x_value, sep = ","), y = input$y_value) +
+                
+                scale_color_gradient(low = "blue", high = "red") +
+                
+                theme_minimal() +
+                theme(panel.grid = element_line(linewidth = .5, color = "black"),
+                      axis.text.x = element_text(size = 15, face = "bold", color = "black", 
+                                                 margin = margin(b = 10)),
+                      axis.text.y = element_text(size = 15, face = "bold", color = "black", 
+                                                 margin = margin(l = 10)),
+                      plot.title = element_text(size = 25, hjust = -.5),
+                      axis.title = element_text(size = 20),
+                      legend.text = element_text(size = 15),
+                      legend.title = element_text(size = 18))
+            print(scatter_plot)}
+        
+        else if (input$plot_type == "pie") {
+            pie_plot <- ggplot(plot_dt, aes_string(x = "", y = input$y_value, fill = input$x_value)) +
+                geom_bar(stat = "identity", linewidth = 2, color = "white") +
+                coord_polar(theta = "y") +
+            labs(title = paste(input$y_value, "Pie Chart"), y = input$y_value, fill = input$x_value) +
+                
+                theme_minimal() +
+                theme(plot.title = element_text(size = 25, hjust = -.5),
+                      legend.text = element_text(size = 15),
+                      legend.title = element_text(size = 18))
+            print(pie_plot)}})
     
-    plot_dt <- clean_dt() %>%
-        select(all_of(c(input$x_value, input$y_value)))
-    
-    if (input$plot_type == "bar") {}
-    
-    else if (input$plot_type == "scatter") {}
-    
-    else if (input$plot_type == "pie") {}})
-
     output$plot_vis <- renderImage({
         req(dt(), input$animated == "Yes")
-    
-    plot_dt <- clean_dt() %>%
-        select(all_of(input$x_value, input$y_value))})
+        
+        plot_dt <- clean_dt() %>%
+            select(all_of(input$x_value, input$y_value))
+
+        bp_animated <- bar_plot
+
+        sp_animated
+
+        pp_animated 
+        
+        })
