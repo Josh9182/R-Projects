3d0c87f6ffcb616f2c3a42e4c726c639827594f2
Josh Lewis <jlewis200413@gmail.com>
2024-07-19 14:13:53 -0600

    Update dt_cleaning.R

--- Sample Data/R Files/dt_cleaning.R
+++ Sample Data/R Files/dt_cleaning.R
@@ -3,7 +3,6 @@ clean_dt <- reactive({
     
         validate(need(nrow(dt()) > 0 && ncol(dt()) > 0, 
                       "Data table must have at least 1 row and 1 column."))
-    
         dt() %>%
             na.omit() %>%
             unique() %>%
