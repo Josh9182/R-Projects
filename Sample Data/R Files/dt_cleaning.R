395c60d3873edb6b4fb0bd17d5df860563e561b9
Josh Lewis <jlewis200413@gmail.com>
2024-07-19 15:24:31 -0600

    Delete Sample Data/R Files/dt_cleaning.R

--- Sample Data/R Files/dt_cleaning.R
+++
@@ -1,9 +0,0 @@
-clean_dt <- reactive({
-        req(dt())
-    
-        validate(need(nrow(dt()) > 0 && ncol(dt()) > 0, 
-                      "Data table must have at least 1 row and 1 column."))
-        dt() %>%
-            na.omit() %>%
-            unique() %>%
-            mutate(across(where(is.character)), ~ str_to_lower(trimws(.x)))})
