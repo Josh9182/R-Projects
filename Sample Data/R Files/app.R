5b2100e52847c255e0339900a7e0e11edeb89c97
Josh Lewis <jlewis200413@gmail.com>
2024-07-19 09:39:43 -0600

    Update app.R

--- Sample Data/R Files/app.R
+++ Sample Data/R Files/app.R
@@ -8,7 +8,6 @@ library(tidyverse)
 library(jsonlite)
 library(readxl)
 library(readODS)
-library(hunspell)
 library(lubridate)
 
 ui <- fluidPage(
@@ -45,15 +44,14 @@ server <- function(input, output, session) {
     
     clean_dt <- reactive({
         req(dt())
-        
-        if (!is.null(dt()) && nrow(dt()) > 0 && ncol(dt()) > 0) {
-            dt() %>%
-                na.omit() %>%
-                mutate_if(is.character, function(x) str_to_lower(hunspell_suggest(trimws(x)))) %>%
-                unique() %>%
-                mutate_if(is.character, function(x) str_replace_all(x, ",", ""))}
-        else {
-            stop("Empty Dataframe unable to visualize, please upload different file.")}})
+    
+        validate(need(nrow(dt()) > 0 && ncol(dt()) > 0, 
+                      "Data table must have at least 1 row and 1 column."))
+    
+        dt() %>%
+            na.omit() %>%
+            unique() %>%
+            mutate(across(where(is.character)), ~ str_to_lower(trimws(.x)))})
     
     observe({
         req(clean_dt())
