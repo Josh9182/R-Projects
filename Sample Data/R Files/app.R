5c0d2111f22a2e77366b8eed02962ee2dc2fec99
Josh Lewis <jlewis200413@gmail.com>
2024-07-19 11:55:48 -0600

    Update app.R

--- Sample Data/R Files/app.R
+++ Sample Data/R Files/app.R
@@ -47,7 +47,6 @@ server <- function(input, output, session) {
     
         validate(need(nrow(dt()) > 0 && ncol(dt()) > 0, 
                       "Data table must have at least 1 row and 1 column."))
-    
         dt() %>%
             na.omit() %>%
             unique() %>%
