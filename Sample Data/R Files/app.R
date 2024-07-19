f0c214910aea36f8c89aec5f684d86eaf4456145
Josh Lewis <jlewis200413@gmail.com>
2024-07-19 15:04:39 -0600

    Update app.R

--- Sample Data/R Files/app.R
+++ Sample Data/R Files/app.R
@@ -45,8 +45,10 @@ server <- function(input, output, session) {
     clean_dt <- reactive({
         req(dt())
     
-        validate(need(nrow(dt()) > 0 && ncol(dt()) > 0, 
-                      "Data table must have at least 1 row and 1 column."))
+        validate(
+            need(nrow(dt()) > 0, "Data table must have at least 1 row."),
+            need(ncol(dt()) > 0, "Data table must have at least 1 column."))
+            
         dt() %>%
             na.omit() %>%
             unique() %>%
