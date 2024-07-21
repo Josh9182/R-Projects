0b7daad2001022190604cfad78b765037e3511ff
Josh Lewis <jlewis200413@gmail.com>
2024-07-21 14:41:25 -0600

    Update data_cleaner.R

--- Sample Data/R Files/data_cleaner.R
+++ Sample Data/R Files/data_cleaner.R
@@ -33,4 +33,47 @@ server <- function(input, output, session) {
                xls <- read_xlsx(input$file$datapath),
                ods <- read_ods(input$file$datapath), 
                stop("Incorrect file type, please restart."))})
+
+server <- function(input, output, session) {
+    data <- reactive({
+        req(input$file)
+        file_ext <- file_ext(input$file$datapath)
+        
+        switch(file_ext, 
+               csv <- read_csv(input$file$datapath),
+               json <- fromJSON(input$file$datapath),
+               xml <- read_xml(input$file$datapath),
+               xls <- read_xlsx(input$file$datapath),
+               ods <- read_ods(input$file$datapath), 
+               stop("Incorrect file type, please restart."))})
+    
+    output$uio <- renderUI({
+        req(input$cols, input$rows)
+        
+        ui_cols <- list()
+        ui_rows <- list()
+        
+        if (input$cols == "yes") {
+            ui_cols <- c(ui_cols, list(selectInput("col_choice", "Select Columns to Remove:", choices = colnames(data()))))}
+        
+        else {NULL}
+        
+        if (input$rows == "yes") {
+            ui_rows <- c(ui_rows, list(sliderInput("row_choice", "Select Rows to Remove:", min = 1, max = nrow(data()))))}
+        
+        else {NULL}
+        
+        
+        ui_elements = c(ui_cols, ui_rows)
+        do.call(tagList, ui_elements)})
+
+    filtered_dt <- reactive({
+        req(data())
+        dt <- data()
+
+        if (!is.null(input$col_choice)) {
+            dt <- dt %>%
+            select(-all_of(input$col_choice))}
+    })
+        
     }
