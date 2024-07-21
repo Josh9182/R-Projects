3c134550408eca65d963337f519c876768fc389e
Josh Lewis <jlewis200413@gmail.com>
2024-07-21 13:37:04 -0600

    Update data_cleaner.R

--- Sample Data/R Files/data_cleaner.R
+++ Sample Data/R Files/data_cleaner.R
@@ -13,10 +13,10 @@ ui <- fluidPage(
     sidebarLayout(
         sidebarPanel(
             fileInput("file", "Import your CSV, JSON, XML, XLSX, or ODS file below:"), 
-            selectInput("white", "Trim white space?", choices = NULL), 
-            selectInput("null", "Remove NULL values?", choices = NULL), 
-            checkboxGroupInput("cols", "Remove certain columns?", choices = NULL),
-            selectInput("row_choice", "Remove certain rows?", choices = NULL)), 
+            selectInput("white", "Trim white space?", choices = c("yes" = "Yes", "no" = "No")), 
+            selectInput("null", "Remove NULL values?", choices = c("yes" = "Yes", "no" = "No")), 
+            selectInput("cols", "Remove certain columns?", choices = c("yes" = "Yes", "no" = "No")),
+            selectInput("row_choice", "Remove certain rows?", choices = c("yes" = "Yes", "no" = "No"))), 
         mainPanel(
             uiOutput("ui"),
             dataTableOutput("table"))))
@@ -27,6 +27,10 @@ server <- function(input, output, session) {
         file_ext <- file_ext(input$file$datapath)
         
         switch(file_ext, 
-               )
-    })
-}
+               csv <- read_csv(input$file$datapath),
+               json <- fromJSON(input$file$datapath),
+               xml <- read_xml(input$file$datapath),
+               xls <- read_xlsx(input$file$datapath),
+               ods <- read_ods(input$file$datapath), 
+               stop("Incorrect file type, please restart."))})
+    }
