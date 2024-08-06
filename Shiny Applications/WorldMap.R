f5352dd74bd3b20e05a4806917d38c9b38bd92a2
Josh Lewis <jlewis200413@gmail.com>
2024-08-06 15:01:15 -0600

    Update WorldMap.R

--- Shiny Applications/WorldMap.R
+++ Shiny Applications/WorldMap.R
@@ -20,7 +20,22 @@ server <- function(input, output) {
         
         if (input$view_type != "--No Selection" && input$view_type == "Specific Location") {
             tagList(
-               textInput("location", "Location:"))}
-    })}
+               textInput("location", "Location:"))}})
+    
+    output$leaf <- renderLeaflet({
+        req(input$view_type)
+        req(input$view_sidebar)
+        
+        if (input$view_type == "--No Selection") {
+            leaflet()}
+        else if (input$view_type != "--No Selection" && input$view_type == "Worldwide") {
+            leaflet() %>%
+                addTiles() %>%
+                setView(lng = 43.66253690456744, lat = -10.029940624081375, zoom = 10)}
+        else if (input$view_type != "--No Selection" && input$location != "") {
+            
+        }
+    })
+}
 
 shinyApp(ui, server)
