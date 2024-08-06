43555ed9621a2474ba92aa66ee294e9e6febb684
Josh Lewis <jlewis200413@gmail.com>
2024-08-06 13:10:57 -0600

    Update WorldMap.R

--- Shiny Applications/WorldMap.R
+++ Shiny Applications/WorldMap.R
@@ -1,1 +1,22 @@
+library(tidyverse)
+library(shiny)
+library(shinyjs)
+library(leaflet)
 
+ui <- fluidPage(
+    titlePanel("Map Viewer"), 
+    
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("view_type", "View Type:", choices = c("--No Selection","Worldwide", "Specific Location"), selected = "--No Selection"), 
+            uiOutput("view_sidebar"), 
+            uiOutput("download")), 
+        mainPanel(
+            leafletOutput("leaf", height = "900px", width = "1200px"))))
+
+server <- function(input, output) {
+    output$leaf <- renderLeaflet({
+        leaflet() %>%
+            addTiles()})}
+
+shinyApp(ui, server)
